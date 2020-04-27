b0ecd1d3a042c3307c4b5f91e5ee332b6c8ce5d2
glenn-dev <glenn.marcano90@gmail.com>
2020-04-27 13:16:40 -0400

    Express API initialized with CRUD methods on user resource.

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+//// EXAMPLES:
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
+// app.put('/user', function (req, res) {
+//   res.send('Got a PUT request at /user');
+// });
+
+// app.delete('/user', function (req, res) {
+//   res.send('Got a DELETE request at /user');
+// });
