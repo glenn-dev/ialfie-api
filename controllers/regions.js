14d3682f11389f14b468f66e6e1e842eaa341c2a
glenn-dev <glenn.marcano90@gmail.com>
2020-06-26 15:25:38 -0400

    Region controller fixed

--- controllers/regions.js
+++ controllers/regions.js
@@ -2,33 +2,61 @@ const pool = require('../database/db');
 
 /* GET REGIONS */
 const getRegions = (req, res) => {
-  pool.query('SELECT * FROM regions ORDER BY name ASC;', (error, results) => {
-    if (error) {
-      throw error;
-    }
-    res.status(200).json(results.rows);
-  });
-};
-
-/* CREATE REGION */
-const createRegion = (req, res) => {
-  const { name } = req.body;
+  const { country_id } = req.body;
   pool.query(
-    `INSERT INTO regions (name) VALUES ${name}`,
+    `
+    SELECT 
+      re.id, 
+      re.region, 
+      co.country 
+    FROM 
+      regions
+      AS re 
+    INNER JOIN 
+      countries
+      AS co
+      ON re.country_id = co.id
+    WHERE
+      re.country_id = ${country_id}
+    ORDER BY 
+      re.region ASC;`,
     (error, results) => {
       if (error) {
         throw error;
       }
-      res.status(201).send(`Region "${name}" added successfully.`);
+      res.status(200).json(results.rows);
+    }
+  );
+};
+
+/* CREATE REGION */
+const createRegion = (req, res) => {
+  const { region, country_id } = req.body;
+  pool.query(
+    `INSERT INTO regions (region, country_id) VALUES ($1, $2)`,
+    [region, country_id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`Region "${region}" added successfully.`);
     }
   );
 };
 
 /* UPDATE REGION */
 const updateRegion = (req, res) => {
-  const { id, name } = req.body;
+  const { id, region, country_id } = req.body;
   pool.query(
-    `UPDATE regions SET name = ${name} WHERE id = ${id}`,
+    `
+    UPDATE 
+      regions 
+    SET 
+      region = $1,
+      country_id = $2,
+    WHERE 
+      id = $3`,
+    [region, country_id, id]
     (error, results) => {
       if (error) {
         throw error;
